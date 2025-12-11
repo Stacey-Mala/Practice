7e2ce3962ace9089bc9c3cdaa4a38365706cc2b5
Stacey Mala <staceymalakhova@gmail.com>
2025-12-11 23:23:42 +0300

    Initial commit

---
+++ Book/Book.java
@@ -0,0 +1,35 @@
+package Book;
+
+public class Book {
+
+    // fields
+    private String title;
+    private String author;
+
+    // constructor with arguments
+    public Book(String someTitle, String someAuthor) {
+        this.title = someTitle;
+        this.author = someAuthor;
+    }
+
+    // getters for title and author fields
+    public String getTitle() {
+        return this.title;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
+
+    // setters to update title and author in an object
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+    public void setAuthor(String newAuthor) {
+        this.author = newAuthor;
+    }
+
+    // print the info out
+    public void printInfo() {
+        System.out.println("Название книги: " + getTitle() + ", автор книги: " + getAuthor());
+    }
+}
---
+++ StudentGroup/Main.java
@@ -0,0 +1,20 @@
+package StudentGroup;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentGroup group1 = new StudentGroup("Buzz", 12);
+        StudentGroup group2 = new StudentGroup("Fuzz", 11);
+
+        group1.printInfo();
+        group2.printInfo();
+
+        System.out.println();
+        System.out.println("Updated");
+
+        group1.setStudentCount(10);
+        group2.setStudentCount(13);
+
+        group1.printInfo();
+        group2.printInfo();
+    }
+}
